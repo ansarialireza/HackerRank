25c724e96f6eec1dbecb668e8b11c22ae00bfa7e
alireza <58270983+ansarialireza@users.noreply.github.com>
2023-11-28 15:38:58 +0330

    simpleArraySum

---
+++ simpleArraySum.py
@@ -0,0 +1,15 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+def simpleArraySum(ar):
+    # Write your code here
+    return sum(ar)
+
+ar=[1,2,3]
+result = simpleArraySum(ar)
+print(result)
